98cd4c919a000f0521f810de8ebfbddaa509362c
Anirudh <ani_707@proton.me>
2026-05-21 16:58:20 +0530

    First Conditional :))

---
+++ Python_Conditionals/01.py
@@ -0,0 +1,10 @@
+number = float(input("Enter the number: "))
+
+if number > 0:
+    print (f"{number} is positive!")
+elif number == 0:
+    print ("Its Zero!")
+elif number <0 :
+    print (f"{number} is negative!")
+else:
+    print ("Source code doesn't have this instance :)")
