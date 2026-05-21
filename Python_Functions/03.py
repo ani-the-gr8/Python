52cdeb7c93246af32634e4482ab9f61a1a65e091
Anirudh <ani_707@proton.me>
2026-05-21 18:32:16 +0530

    Even_odd function + Interactive

---
+++ Python_Functions/03.py
@@ -0,0 +1,10 @@
+
+def even_odd(a):
+    if a %2 == 0:
+        print("The number is even")
+    elif a % 2 != 0:
+        print("The number is odd") 
+
+num = float(input("Enter the number: "))
+even_odd(num)
+
