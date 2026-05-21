caf59f05c85e892ecfd6940025225d0fc980531e
Anirudh <ani_707@proton.me>
2026-05-21 16:51:22 +0530

    String capitalizer and counter

---
+++ Python_Basics/04.py
@@ -0,0 +1,8 @@
+string = input("Enter the string: ")
+
+capitalized = string.upper()
+length = len(string)
+
+print (f"Your Capitalized string: {capitalized}")
+print (f"Number of characters: {length}")
+
