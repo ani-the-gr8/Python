6de039e44e9771fc73a932f80b2412942d4afcc2
Anirudh <ani_707@proton.me>
2026-05-21 17:05:20 +0530

    Grader

---
+++ Python_Conditionals/02.py
@@ -0,0 +1,10 @@
+marks = float(input("Enter your Marks: "))
+
+if marks >= 90:
+    print("Go Enjoy! You have got an A")
+elif marks >=75 and marks <=89:
+    print ("You got a B dude XD")
+elif marks>=50 and marks <=74:
+    print ("Try more, and get more than C :)")
+else: 
+    print ("Go work in McDonalds")
