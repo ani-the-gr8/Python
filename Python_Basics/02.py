b64f165845951a8cecbdfb507a6ff241ec2a071f
Anirudh <ani_707@proton.me>
2026-05-21 11:20:14 +0530

    My second program!

---
+++ Python_Basics/02.py
@@ -0,0 +1,12 @@
+num_1 = int and float(input("Enter the first number: "))
+num_2 = int and float(input("Enter the second number: "))
+
+sum = num_1 + num_2
+difference = num_1 - num_2
+product = num_1 * num_2
+division = num_1 / num_2
+
+print(f"The sum is {sum}")
+print(f"The difference is {difference}")
+print(f"The product is {product}")
+print(f"The division is {division}")
