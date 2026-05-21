2fa71d9c9bf791d37dcff1235ec35a1288cf68dc
Anirudh <ani_707@proton.me>
2026-05-21 17:11:15 +0530

    odd or even?

---
+++ Python_Conditionals/03.py
@@ -0,0 +1,8 @@
+number = float(input("Enter the number: "))
+
+if number % 2 == 0:
+    print (f"{number} is even!")
+elif number%2 != 0:
+    print (f"{number} is odd!")
+else:
+    print ("The source code doesn't have this ooga booga number")
